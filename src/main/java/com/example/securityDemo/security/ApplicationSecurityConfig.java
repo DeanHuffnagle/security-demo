b79c2241ddf0a5e54ac52611bba26c2908882bdd
deanhuffnagle <huffnagleDean@gmail.com>
2022-06-27 22:54:55 -0400

    Filters and Stateless Sessions

--- src/main/java/com/example/securityDemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityDemo/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.securityDemo.security;
 
 import com.example.securityDemo.auth.ApplicationUserService;
+import com.example.securityDemo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -36,32 +38,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .csrf()
         .disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
         .authorizeRequests()
         .antMatchers("/", "index", "/css/*", "/js/*")
         .permitAll()
         .antMatchers("/api/**")
         .hasAnyRole(STUDENT.name())
         .anyRequest()
-        .authenticated()
-        .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .defaultSuccessUrl("/courses", true)
-            .passwordParameter("password")
-            .usernameParameter("username")
-        .and()
-        .rememberMe()
-            .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-            .key("somethingsupersecure")
-            .rememberMeParameter("remember-me")
-        .and()
-        .logout()
-            .logoutUrl("/logout")
-            .clearAuthentication(true)
-            .invalidateHttpSession(true)
-            .deleteCookies("JSESSIONID", "remember-me")
-            .logoutSuccessUrl("/login");
+        .authenticated();
   }
 
   @Override
